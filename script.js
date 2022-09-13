ff04775205442b84454398de737227bdeeb16d08
GreatApe78 <111906738+GreatApe78@users.noreply.github.com>
2022-09-13 18:08:29 -0300

    site
    site

---
+++ script.js
@@ -0,0 +1,560 @@
+
+var web3 = new Web3(ethereum);
+
+
+
+const contract_abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "burn",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "burnFrom",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "subtractedValue",
+                "type": "uint256"
+            }
+        ],
+        "name": "decreaseAllowance",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "addedValue",
+                "type": "uint256"
+            }
+        ],
+        "name": "increaseAllowance",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "mint",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+const contract_address = "0x5B0ae342Da07F0E76400529D56021a1f3CceE8b8"
+
+var ver_FLM = document.getElementById("ver_FLM");
+
+var mostrar_FLM = document.getElementById("mostrar_FLM")
+
+
+var ver_dono = document.getElementById("ver_dono")
+
+var mostrar_dono = document.getElementById("aparece_hash_dono")
+
+
+var ver_contrato = document.getElementById("ver_contrato")
+
+var mostrar_contrato = document.getElementById("mostrar_contrato")
+
+
+var view_FLM = document.getElementById("view_FLM")
+var show_balance = document.getElementById("show_balance")
+async function ver_quantidade_de_FLM() {
+
+    if (ethereum) {
+        try {
+            var ktr = new web3.eth.Contract(contract_abi, contract_address)
+
+            var chamada = await ktr.methods.totalSupply().call()
+            return chamada
+        }
+        catch (error) {
+            console.log('erro no ver nome', error)
+        }
+    }
+    else {
+        alert('instale o metamask.')
+    }
+
+
+}
+
+
+
+
+
+ver_FLM.addEventListener("click", () => {
+    ver_quantidade_de_FLM().then((response) => {
+
+        mostrar_FLM.innerHTML = `<h1>${Number(response) / (10 ** 18)}</h1> `
+
+
+
+    }).catch((error) => {
+        console.log("erro no no catch do eventlistener", error)
+
+    })
+
+
+})
+
+async function pegar_hash_dono() {
+
+    if (ethereum) {
+        try {
+            var ktr = new web3.eth.Contract(contract_abi, contract_address)
+
+            var chamada = ktr.methods.owner().call()
+            return chamada
+        }
+        catch (error) {
+
+            console.log(error)
+        }
+    }
+    else {
+        alert('instale o metamask.')
+    }
+}
+
+
+
+ver_dono.addEventListener("click", () => {
+
+    pegar_hash_dono().then((response) => {
+
+        mostrar_dono.innerHTML = `<h1>${response}</h1>`
+
+    }).catch((error) => {
+
+        console.log("deu erro aqui no hash dono", error)
+    })
+
+
+})
+
+
+
+async function get_contract_address() {
+    if (ethereum) {
+        try {
+            return contract_address
+        }
+        catch (error) {
+            console.log("deu erro no get contract address", error)
+        }
+    } else {
+
+        alert('instale o metamask')
+    }
+
+}
+
+
+ver_contrato.addEventListener("click", () => {
+    get_contract_address().then((response) => {
+
+        mostrar_contrato.innerHTML = `<h1>${response}</h1>`
+
+
+
+
+    }).catch((error) => {
+        console.log("erro no ver contrato", error);
+    })
+
+
+
+
+
+})
+
+
+async function ShowBalance() {
+    if (ethereum) {
+        try {
+            let accounts = await ethereum.request({ method: "eth_requestAccounts" })
+            let carteira = accounts[0]
+            var ktr = new web3.eth.Contract(contract_abi, contract_address)
+            var chamada = await ktr.methods.balanceOf(carteira).call()
+            return chamada
+
+
+
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    } else {
+        alert('Instale o Metamask!')
+    }
+
+
+
+
+
+}
+//wd
+view_FLM.addEventListener("click", () => {
+
+    ShowBalance().then((chamada) => {
+        show_balance.innerHTML = ((chamada) / 10 ** 18).toString()
+    }).catch((error) => {
+        console.log(error)
+    })
+
+
+
+
+
+})
